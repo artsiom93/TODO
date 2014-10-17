e60cd712c9f20027b6944929da2a45ff8944bfce
artsiom93 <suhoverartyom@gmail.com>
2014-10-17 21:11:33 +0400

    2

--- TODOApp/src/application/Main.java
+++ TODOApp/src/application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			// Person person = new Person();
 			// person.setFirstname("Artyom");
 			// person.setLastname("asd");
